fd20d63e383e3d7e18ffe01677b356b448a4affb
Michael Coaguila <maicolfl@gmail.com>
2024-09-10 22:46:53 -0500

    Add routes for login, welcome, course selection, enrollment confirmation, enrolled schedule, schedule preview, and schedule simulation components

--- MatriculaWEB/src/app/app.routes.ts
+++ MatriculaWEB/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { CourseSelectionComponent } from './pages/course-selection/course-selection.component';
+import { EnrollmentConfirmationComponent } from './pages/enrollment-confirmation/enrollment-confirmation.component';
+import { EnrolledScheduleComponent } from './pages/enrolled-schedule/enrolled-schedule.component';
+import { SchedulePreviewComponent } from './pages/schedule-preview/schedule-preview.component';
+import { ScheduleSimulationComponent } from './pages/schedule-simulation/schedule-simulation.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "login", component: LoginComponent },
+    { path: "welcome", component: WelcomeComponent},
+    { path: "course-selection", component: CourseSelectionComponent},
+    { path: "enrollment-confirmation", component: EnrollmentConfirmationComponent},
+    { path: "enrolled-schedule", component: EnrolledScheduleComponent},
+    { path: "not-found", component: LoginComponent},
+    { path: "schedule-preview", component: SchedulePreviewComponent},
+    { path: "schedule-simulation", component: ScheduleSimulationComponent}
+];
